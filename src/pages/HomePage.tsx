162424f7a247ef5b4772e68aec06f4e9aae85d95
Cdr-Jie <CHANZHIJIE_BI23@iluv.ums.edu.my>
2026-05-11 15:31:23 +0800

    More fixes

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PrayerTimesCarousel from '../components/PrayerTimesCarousel';
 import Banner from '../components/Banner';
 import MasjidLocation from '../components/MasjidLocation';
